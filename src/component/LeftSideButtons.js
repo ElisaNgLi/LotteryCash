1c20b94eb6b5232a9ddc77f7503d94bb1335f11b
Elisa <elisangli11@gmail.com>
2021-06-23 21:20:53 -0400

    Add button functionalities

--- src/component/LeftSideButtons.js
+++ src/component/LeftSideButtons.js
@@ -1,43 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../css/LeftSideButtons.css";
 import lottery from "../assets/lottery-balls.png";
 
-const LeftSideButtons = (props) => {
+const LeftSideButtons = () => {
+  const [total, setTotal] = useState(0);
+
+  //When user click the button it adds up the value
+  const addTotal = (value) => {
+    const totalValue = value + total;
+    setTotal(totalValue);
+  };
+
+  // Dollar value of the button
+  const buttonOption = [1, 5, 10, 20];
+
   return (
     <div className="container">
       <div id="image-container" className="col">
         <img src={lottery} alt="Lottery" />
       </div>
       <div id="button-container" className="col">
-        <div className="row">
-          <div className="col">
-            <button id="btn-container" type="button" className="btn btn-lg">
-              {" "}
-              $1
-            </button>
-          </div>
-          <div className="col">
-            <button id="btn-container" type="button" className="btn btn-lg">
-              {" "}
-              $5
-            </button>
-          </div>
-        </div>
-        <div id="second-row" className="row">
-          <div className="col">
-            <button id="btn-container" type="button" className="btn btn-lg">
-              {" "}
-              $10
-            </button>
-          </div>
-          <div className="col">
-            <button id="btn-container" type="button" className="btn btn-lg">
-              {" "}
-              $20
-            </button>
-          </div>
-        </div>
+        {buttonOption.map((value) => (
+          <button
+            key={value.toString()}
+            id="btn-container"
+            type="button"
+            className="btn btn-lg"
+            onClick={() => addTotal(value)}
+          >
+            ${value}
+          </button>
+        ))}
       </div>
     </div>
   );
