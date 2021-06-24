e552beaca416c0ad2be706106be03e4b2049c2e6
Elisa <elisangli11@gmail.com>
2021-06-23 21:21:10 -0400

    Update design

--- src/component/Ticket.js
+++ src/component/Ticket.js
@@ -2,19 +2,31 @@ import React from "react";
 
 import "../css/Ticket.css";
 const Ticket = (props) => {
+  //Generate the ticket number 1-20
+  const ticketNumber = [...Array(20).keys()].map((i) => i + 1);
+
   return (
     <div id="ticket-container" className="card">
       <div className="card-body">
-        <div>
-          <p>some numbers here</p>
+        <div className="container">
+          {ticketNumber.map((value) => (
+            <button
+              key={value.toString()}
+              id="number-container"
+              type="button"
+              className="btn"
+            >
+              {value}
+            </button>
+          ))}
         </div>
         <div className="container">
           <div className="row">
-            <button type="button" className="btn">
+            <button id="button-con" type="button" className="btn">
               {" "}
               CASH
             </button>
-            <button type="button" className="btn">
+            <button id="button-con" type="button" className="btn">
               {" "}
               CLEAR
             </button>
